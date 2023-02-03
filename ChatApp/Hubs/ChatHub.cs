28e12f7284371bc8642afd578d83d37e053724eb
stanslavshev <subjectzy@gmail.com>
2023-02-03 20:24:53 +0900

    init

---
+++ ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Hubs
+{
+
+    [Authorize]
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message, string to)
+        {
+            try
+            {
+                // получение текущего пользователя, который отправил сообщение
+                //var userName = Context.UserIdentifier;
+                if (Context.UserIdentifier is string userName)
+                {
+                    await Clients.Users(to, userName).SendAsync("Receive", message, userName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+      
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("Notify", $"Приветствуем {Context.UserIdentifier}");
+            await base.OnConnectedAsync();
+        }
+    }
+
+}
+
---
+++ ChatApp/Program.cs
@@ -0,0 +1,114 @@
+using ChatApp.Hubs;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var people = new List<Person>
+ {
+    new Person("tom@gmail.com", "11111"),
+    new Person("bob@gmail.com", "55555"),
+    new Person("sam@gmail.com", "22222")
+};
+
+builder.Services.AddSingleton<IUserIdProvider, CustomUserIdProvider>();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = AuthOptions.ISSUER,
+            ValidateAudience = true,
+            ValidAudience = AuthOptions.AUDIENCE,
+            ValidateLifetime = true,
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+            ValidateIssuerSigningKey = true
+        };
+
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                var accessToken = context.Request.Query["access_token"];
+
+                // если запрос направлен хабу
+                var path = context.HttpContext.Request.Path;
+                if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/chat"))
+                {
+                    // получаем токен из строки запроса
+                    context.Token = accessToken;
+                }
+                return Task.CompletedTask;
+            }
+        };
+    });
+
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy",
+        builder =>
+        {
+            builder.AllowAnyHeader()
+                   .AllowAnyMethod()
+                   .SetIsOriginAllowed((host) => true)
+                   .AllowCredentials();
+        }));
+
+var app = builder.Build();
+
+
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseCors("CorsPolicy");
+
+app.MapHub<ChatHub>("/chat");
+
+app.MapPost("/login", (Person loginModel) =>
+{
+    // находим пользователя 
+    Person? person = people.FirstOrDefault(p => p.Email == loginModel.Email && p.Password == loginModel.Password);
+    // если пользователь не найден, отправляем статусный код 401
+    if (person is null) return Results.Unauthorized();
+
+    var claims = new List<Claim> { new Claim(ClaimTypes.NameIdentifier, person.Email) };
+    // создаем JWT-токен
+    var jwt = new JwtSecurityToken(
+            issuer: AuthOptions.ISSUER,
+            audience: AuthOptions.AUDIENCE,
+            claims: claims,
+            expires: DateTime.UtcNow.Add(TimeSpan.FromDays(14)),
+            signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+    var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+    // формируем ответ
+    var response = new
+    {
+        access_token = encodedJwt,
+        username = person.Email
+    };
+
+    return Results.Json(response);
+});
+
+
+app.Run();
+
+public class AuthOptions
+{
+    public const string ISSUER = "MyAuthServer"; // издатель токена
+    public const string AUDIENCE = "MyAuthClient"; // потребитель токена
+    const string KEY = "mysupersecret_secretkey!123";   // ключ для шифрации
+    public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
+        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+}
+
+record class Person(string Email, string Password);
